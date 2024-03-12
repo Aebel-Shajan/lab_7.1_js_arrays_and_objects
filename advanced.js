9273d45f461e69d99a9b918f2cf24ffd40a35733
Aebel Shajan <aebelshajan@gmail.com>
2024-03-12 09:50:21 +0000

    Complete advanced.js exercises

--- advanced.js
+++ advanced.js
@@ -32,26 +32,32 @@ unitedKingdom = [
  * Q1. Add some tourist attractions to England
  */
 
-england = null;
 
-// console.log(england);
+england = unitedKingdom.filter((element) => element.name == "England")[0];
+england.touristAttractions = ["Big Ben", "Buckingham Palace"];
+console.log(england);
 
 /**
  * Q2. Change the capital of Wales to "Cardiff"
  */
 
-wales = null;
-
-// console.log(wales);
+wales = unitedKingdom.filter((element) => element.name == "Wales")[0];
+wales.capital = "Cardiff";
+console.log(wales);
 
 /**
  * Q3. Find a method in the documentation which will give you all of the available keys for Northern Ireland
  */
-
-northernIrelandKeys = null;
-
-// console.log(northernIrelandKeys);
+northernIreland = unitedKingdom.filter((element) => element.name == "Northern Ireland")[0];
+northernIrelandKeys = Object.keys(northernIreland);
+console.log(northernIrelandKeys);
 
 /**
  * Q4. Use an if statement to compare the population of Scotland to the population of Wales, and say which is bigger.
  */
+scotland = unitedKingdom.filter((element) => element.name == "Scotland")[0];
+if (scotland.population > wales.population) {
+  console.log(scotland.name + " has bigger populations");
+} else {
+  console.log(wales.name + " has bigger popultation");
+}
